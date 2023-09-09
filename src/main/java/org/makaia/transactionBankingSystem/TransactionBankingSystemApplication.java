e1170a726273ffad624dffc434f791cb7ecde8c4
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-09-09 14:28:06 -0500

    Estructura inicial del proyecto y su conexión a la base de datos.

---
+++ src/main/java/org/makaia/transactionBankingSystem/TransactionBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.makaia.transactionBankingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBankingSystemApplication.class, args);
+	}
+
+}
