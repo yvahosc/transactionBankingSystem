73ca65464287a64c3a09ea80147abe7c9e5b210a
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-09-24 21:43:18 -0500

    Revert "Creación de la estructura para la obtención, creación y actualización de las personas propietarias de las cuentas bancarias."
    This reverts commit 57fc107a0e64152f214dafb01a7f36e31f5a67c3.

--- src/main/java/org/makaia/transactionBankingSystem/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.makaia.transactionBankingSystem.repository;
-
-import org.makaia.transactionBankingSystem.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, String> {
-}
